b64bbc5575c60475824ce424e7a76a85c32ecfb6
deadvexon <299763@ust.com>
2025-08-13 20:47:14 +0000

    edit setup bash file

--- setup.sh
+++ setup.sh
@@ -33,3 +33,17 @@ echo "Kubernetes control-plane setup complete!"
 echo "Cluster status:"
 sudo -u ubuntu kubectl get nodes
 sudo -u ubuntu kubectl get pods --all-namespaces
+
+git clone https://github.com/deadvexon/find-your-bias
+sleep 20
+
+kubectl apply -f find-your-bias/k8s-specifications
+sleep 10
+
+# setting up the runner 
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux-x64-2.327.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.327.1/actions-runner-linux-x64-2.327.1.tar.gz
+echo "d68ac1f500b747d1271d9e52661c408d56cffd226974f68b7dc813e30b9e0575  actions-runner-linux-x64-2.327.1.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-linux-x64-2.327.1.tar.gz
+./config.sh --url https://github.com/deadvexon/find-your-bias --token BVV4AU644OQINJKLP3W7NBDITUDT4
+./run.sh
